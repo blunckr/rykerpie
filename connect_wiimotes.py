447e5c77c0d891624439972c93d5d45b4dd7920f
Ryker Blunck <rykerblunck@gmail.com>
2015-07-04 23:42:27 +0000

    init

---
+++ connect_wiimotes.py
@@ -0,0 +1,42 @@
+import re
+import subprocess
+import time
+
+from os import listdir
+
+def get_next_player_number():
+	devs = listdir('/dev/input')
+	js_matcher = re.compile('^js')
+	players = 1
+	for dev in devs:
+		if js_matcher.match(dev):
+			players += 1
+	
+	return players
+
+def create_config_file(player):
+	template_file = open('/home/pi/wminput/gamepad')
+	content = template_file.read()
+	template_file.close()
+
+	new_file = open('/home/pi/wminput/custom_gamepad' + player, 'w')
+	new_file.write(content)
+	new_file.write('\n')
+	new_file.write('Plugin.led.Led' + player + ' = 1')
+	new_file.write('\n')
+	new_file.close()
+
+# recursively calls itself until it fails to find one
+def connect_wiimotes():
+	player = get_next_player_number()
+	create_config_file(str(player))
+
+	process = subprocess.Popen(['wminput', '-c', '/home/pi/wminput/custom_gamepad' + str(player)],
+	                           stdout=subprocess.PIPE)
+	
+	time.sleep(10)
+	result = process.poll()
+	if result == None: # no bad exit code
+		connect_wiimotes()
+
+connect_wiimotes()
---
+++ monitor_wiimotes.py
@@ -0,0 +1,34 @@
+import subprocess
+import time
+
+
+def monitor_wiimotes():
+	failed_once = False
+	while True:
+		running = emulator_running()
+		if failed_once and not running:
+			subprocess.call(['killall', 'wminput'])
+		else: 
+			failed_once = not running
+
+		time.sleep(2)
+
+# if no emulators are running, kill wiimote connections
+def emulator_running():
+	try:
+		es_pid = subprocess.check_output(['pgrep', '-f', '/opt/retropie/supplementary/emulationstation/emulationstation'])
+		es_pid = es_pid.rstrip() # remove newline
+	except:
+		# es isn't running
+		return False
+	else: 
+		try:
+			childen = subprocess.check_output(['pgrep', '-cP', es_pid])
+		except:
+			# no children
+			return False
+		else: 
+			return True	
+
+monitor_wiimotes()
+
